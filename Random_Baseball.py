7b161d4323c932e6ca5193a82995f3dc33c6bf26
jaemin03 <jeongjaemin60@gmail.com>
2022-05-06 14:42:27 +0900

    랜덤 야구게임(수정)

--- Random_Baseball.py
+++ Random_Baseball.py
@@ -1,30 +1,36 @@
 import random
 
+def rand():
+    while True:
+        rand1 = random.randint(0,9)
+        rand2 = random.randint(0,9)
+        rand3 = random.randint(0,9)
+        if rand1!=rand2!=rand3:
+            rand_list.append(str(rand1))
+            rand_list.append(str(rand2))
+            rand_list.append(str(rand3))
+            break
+
 while True:
-    rand1=random.randint(0,9)
-    rand2=random.randint(0,9)
-    rand3 = random.randint(0, 9)
-    if rand1!=rand2!=rand3:
-        break
 
-rand=[]
-rand.append(str(rand1))
-rand.append(str(rand2))
-rand.append(str(rand3))
+    rand_list=[]
+    rand()
+    strike=0
+    ball=0
+    num=input("3자리 숫자 입력 : ")
+    user=list(num)
+    print(rand_list)
+    print(user)
 
-strike=0
-ball=0
-num=input("3자리 숫자 입력 : ")
-user=list(num)
-print(rand)
-print(user)
+    for i in range(len(rand_list)):
+        if user.count(rand_list[i]):
+            if rand_list[i]==user[i]:
+                strike+=1
+            else:
+                ball+=1
 
-for i in range(len(rand)):
-    if user.count(rand[i]):
-        if rand[i]==user[i]:
-            strike+=1
-        else:
-            ball+=1
+    print(strike)
+    print(ball)
 
-print(strike)
-print(ball)
+    if strike==3:
+        print("정답입니다.")
